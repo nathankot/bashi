5626d94b01d9a4be449233c738c7bc5885efd597
Nathan Kot <me@nathankot.com>
2022-12-12 15:54:10 +0900

    Graceful shutdown

--- server/main.ts
+++ server/main.ts
@@ -4,13 +4,30 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
+import { signal } from "https://deno.land/std@0.117.0/signal/mod.ts";
+
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-await start(manifest, {
+// Load clients
+import { gracefulStop, setup } from "@/clients.ts";
+await setup();
+
+const abortController = new AbortController();
+
+start(manifest, {
   port: 8080,
   plugins: [twindPlugin(twindConfig)],
+  signal: abortController.signal,
 });
+
+const sig = signal("SIGTERM", "SIGINT");
+for await (const _ of sig) {
+  console.log("SIGINT or SIGTERM received");
+  abortController.abort("closing");
+  await gracefulStop();
+  Deno.exit(0);
+}

e92fdc48ebb35db5dfc70ff5886d23406ea7ba18
Nathan Kot <me@nathankot.com>
2022-12-14 15:39:48 +0900

    Make the test tabular

--- server/routes/api/session/requests_test.ts
+++ server/routes/api/session/requests_test.ts
@@ -1,17 +1,18 @@
 import { assertSnapshot } from "std/testing/snapshot.ts";
 import { stub } from "std/testing/mock.ts";
 
-import { handlerCtx, session } from "@/fixtures.ts";
+import * as fixtures from "@/fixtures.ts";
 import * as clients from "@/clients.ts";
 import { handler } from "./requests.ts";
 
 const nowFixture = new Date();
 
-Deno.test("/requests - valid completion request", async (t) => {
-  const openAiStub = stub(
-    clients.openai,
-    "createCompletion",
-    async () =>
+for (const test of [
+  {
+    description: "success",
+    request: `{ "request": "a mock request" }`,
+    session: fixtures.session,
+    openAiFn: async () =>
       ({
         data: {
           choices: [
@@ -20,31 +21,36 @@ Deno.test("/requests - valid completion request", async (t) => {
             },
           ],
         },
-      } as any)
-  );
-  try {
-    const enc = new TextEncoder();
-    const req = new Request("http://localhost/", {
-      method: "POST",
-      body: enc.encode(`{ "request": "a mock request" }`),
-    });
-    req.headers.set("Content-Type", "application/json");
-    const response = await handler.POST!(req, {
-      ...handlerCtx,
-      state: {
-        now: nowFixture,
-        clients,
-        session,
-      },
-    });
-    const responseJson = await response.json();
-    await assertSnapshot(t, responseJson);
-  } finally {
-    openAiStub.restore();
-  }
-});
+      } as any),
+  },
+  // invalid session
+  // invalid request
+  // too many tokens
+  // rate limited
+  // openai error
+]) {
+  Deno.test("POST /api/session/requests: " + test.description, async (t) => {
+    const openAiStub = stub(clients.openai, "createCompletion", test.openAiFn);
 
-// invalid session
-// invalid request
-// too many tokens
-// rate limited
+    try {
+      const enc = new TextEncoder();
+      const req = new Request("http://localhost/", {
+        method: "POST",
+        body: enc.encode(test.request),
+      });
+      req.headers.set("Content-Type", "application/json");
+      const response = await handler.POST!(req, {
+        ...fixtures.handlerCtx,
+        state: {
+          now: nowFixture,
+          clients,
+          session: test.session,
+        },
+      });
+      const responseJson = await response.json();
+      await assertSnapshot(t, responseJson);
+    } finally {
+      openAiStub.restore();
+    }
+  });
+}

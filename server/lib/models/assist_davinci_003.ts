21efffabd31b9cc2c417e5250b886d2f405a9702
Nathan Kot <me@nathankot.com>
2022-12-15 22:32:55 +0900

    Shortern model name

--- server/lib/models/assist_davinci_003.ts
+++ server/lib/models/assist_davinci_003.ts
@@ -2,11 +2,11 @@ import * as t from "io-ts";
 
 import { FunctionList } from "@lib/function.ts";
 
-export const AssistDaVinci003ModelOptions = t.type({
+export const AssistDaVinci003Options = t.type({
   model: t.literal("assist-davinci-003"),
   functions: FunctionList,
 });
 
-export type AssistDaVinci003ModelOptions = t.TypeOf<
-  typeof AssistDaVinci003ModelOptions
->;
+export type AssistDaVinci003Options = t.TypeOf<typeof AssistDaVinci003Options>;
+
+export default function run() {}

c365d5ef332b17fe1eb89902385e8fb040f2ee85
Nathan Kot <me@nathankot.com>
2022-12-18 17:52:34 +0900

    Sort dep lines alphabetically

--- server/deps.ts
+++ server/deps.ts
@@ -1,20 +1,20 @@
 // This file is used to load a subset of dependencies via `deno cache` as a early layer in the Dockerfile.
 
-import "openai";
-import "twind";
-import "preact";
-import "preact/hooks";
+import "$fresh/plugins/twind.ts";
+import "$fresh/server.ts";
 import "@preact/signals";
 import "@preact/signals-core";
+import "fp-ts";
 import "https://deno.land/x/dotenv/load.ts";
 import "io-ts";
 import "io-ts-types";
-import "fp-ts";
-import "redis";
 import "mathjs";
+import "openai";
+import "preact";
+import "preact/hooks";
+import "redis";
+import "twind";
 import "typescript-parsec";
-import "$fresh/server.ts";
-import "$fresh/plugins/twind.ts";
 
 // @deno-types="https://esm.sh/@ygoe/msgpack@1.0.3/msgpack.d.ts"
 export { default as msgpack } from "https://esm.sh/@ygoe/msgpack@1.0.3/msgpack.js";

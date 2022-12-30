b6e032858df3c9eee4be5bc54053188adbf61648
Nathan Kot <me@nathankot.com>
2022-12-30 18:35:03 +0900

    Write to openapi.json file

--- server/dev.ts
+++ server/dev.ts
@@ -2,11 +2,11 @@
 
 import dev from "$fresh/dev.ts";
 import updateExamples from "./dev/updateExampleRequests.ts";
-import generateSwaggerSpec from "./dev/generateSwaggerSpec.ts";
+import generateOpenAPISpec from "./dev/generateOpenAPISpec.ts";
 
 const srv = dev(import.meta.url, "./main.ts");
 // update the prompt examples file:
 const e = updateExamples("./static/assist_examples.jsonl");
-const s = generateSwaggerSpec();
+const s = generateOpenAPISpec();
 
 await Promise.all([srv, e, s]);

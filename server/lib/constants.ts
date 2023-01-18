03b4140f08b2d46e4484f5dc55a3a7157cfe0603
Nathan Kot <me@nathankot.com>
2023-01-19 08:23:54 +0900

    Remove unused constant

--- server/lib/constants.ts
+++ server/lib/constants.ts
@@ -1,8 +1,6 @@
 export const SESSION_EXPIRY_MS = 1000 * 60 * 60 * 3;
 export const DEFAULT_MAX_RESPONSE_TOKENS = 1000;
 
-export const WEB_EXAMPLE_MODEL = "assist-001";
-
 export const PROGRAMMING_LANGUAGES = [
   "javascript",
   "typescript",

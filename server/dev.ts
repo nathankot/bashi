21db91be89d3a3a5424e69daf7dcba2195e10844
Nathan Kot <me@nathankot.com>
2023-01-21 10:58:06 +0900

    Add polyfill before dev as well

--- server/dev.ts
+++ server/dev.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env -S deno run --allow-run --allow-write --allow-read --allow-env --allow-net --check --watch=static/,routes/ dev.ts
 
+// Polyfill XMLHttpRequest which is required by the openai client,
+// see: https://github.com/denoland/deno/discussions/15040
+import "xhr-polyfill";
+
 import dev from "$fresh/dev.ts";
 import updateExamples from "./dev/updateExampleRequests.ts";
 import generateOpenAPISpec from "./dev/generateOpenAPISpec.ts";

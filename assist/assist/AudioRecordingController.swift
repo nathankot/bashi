88cce79273e38fa84edea24879f7ba5b9cf76827
Nathan Kot <me@nathankot.com>
2023-02-05 16:18:49 +0900

    Only auth speech recognizer when necessary

--- assist/assist/AudioRecordingController.swift
+++ assist/assist/AudioRecordingController.swift
@@ -11,45 +11,45 @@ import SwiftUI
 import Combine
 
 actor AudioRecordingController {
-    
+
     private var speechRecognizerAuthStatus: SFSpeechRecognizerAuthorizationStatus = .notDetermined
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(
         identifier:
             Locale.current.identifier.starts(with: "en-") ||
             Locale.current.identifier.starts(with: "en_")
             ? Locale.current.identifier
-            : "en-US"))!
-    
-    
+        : "en-US"))!
+
+
     private var engine: AVAudioEngine? = nil
     private var speechRecognitionRequest: SFSpeechAudioBufferRecognitionRequest? = nil
     private var speechRecognitionTask: SFSpeechRecognitionTask? = nil
     private var transcribedSubject: CurrentValueSubject<String, Error>? = nil
-    
+
     public var transcribedPublished: AnyPublisher<String, Error>? {
         return transcribedSubject?.eraseToAnyPublisher()
     }
-    
+
     public var transcribeStream: AsyncThrowingPublisher<AnyPublisher<String, Error>>? {
         guard let published = transcribedPublished else { return nil }
         return AsyncThrowingPublisher(published)
     }
-    
+
     private static func prepareEngine() throws -> (AVAudioEngine, SFSpeechAudioBufferRecognitionRequest) {
         let audioEngine = AVAudioEngine()
-        
+
         let request = SFSpeechAudioBufferRecognitionRequest()
         request.shouldReportPartialResults = true
-        
+
         let inputNode = audioEngine.inputNode
-        
+
         let recordingFormat = inputNode.outputFormat(forBus: 0)
         inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
             request.append(buffer)
         }
         audioEngine.prepare()
         try audioEngine.start()
-        
+
         return (audioEngine, request)
     }
 
@@ -61,11 +61,13 @@ actor AudioRecordingController {
         speechRecognitionTask = nil
         transcribedSubject = nil
     }
-    
+
     func startRecording() async throws -> AsyncThrowingPublisher<AnyPublisher<String, Error>> {
-        speechRecognizerAuthStatus = await withCheckedContinuation { continuation in
-            SFSpeechRecognizer.requestAuthorization { authStatus in
-                continuation.resume(returning: authStatus)
+        if speechRecognizerAuthStatus == .notDetermined {
+            speechRecognizerAuthStatus = await withCheckedContinuation { continuation in
+                SFSpeechRecognizer.requestAuthorization { authStatus in
+                    continuation.resume(returning: authStatus)
+                }
             }
         }
         if speechRecognizerAuthStatus != .authorized {
@@ -78,7 +80,7 @@ actor AudioRecordingController {
             throw AppError.Internal("should not be recording")
         }
         logger.info("starting audio recording")
-        
+
         do {
             let (engine, speechRecognitionRequest) = try Self.prepareEngine()
             self.engine = engine
@@ -102,19 +104,19 @@ actor AudioRecordingController {
                     engine.inputNode.removeTap(onBus: 0)
                 }
             })
-            
+
             return transcribeStream!
         } catch {
             reset()
             throw error
         }
     }
-    
+
     func stopRecording() async throws -> String? {
         logger.info("stopping audio recording")
         engine?.stop()
         speechRecognitionRequest?.endAudio()
-        
+
         var result: String? = nil
         if let transcribeStream = transcribeStream {
             // Use the last transcription, transcription may continue even after
@@ -123,11 +125,11 @@ actor AudioRecordingController {
                 result = transcription
             }
         }
-        
+
         reset()
 
         return result
     }
-    
+
 }
 
